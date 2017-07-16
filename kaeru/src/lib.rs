a89c69315947dee16bc1d1d5a31fd1724f2bb6f6
Luminarys <postmaster@gensok.io>
2017-07-15 21:08:06 -0700

    Prepare for refactoring

---
+++ kaeru/src/lib.rs
@@ -0,0 +1,8 @@
+extern crate ffmpeg_sys;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,18 @@
 #[macro_use]
 extern crate slog;
 extern crate slog_term;
+extern crate slog_async;
 extern crate shout;
-extern crate ffmpeg;
 extern crate libc;
-extern crate hyper;
 extern crate toml;
-extern crate ring_buffer;
+extern crate serde;
 extern crate serde_json;
-extern crate futures_cpupool;
-
 #[macro_use]
 extern crate lazy_static;
 #[macro_use]
-extern crate rustful;
-#[macro_use]
 extern crate serde_derive;
 
+extern crate kaeru;
 
 mod radio;
 mod transcode;
@@ -29,18 +25,18 @@ mod prebuffer;
 use std::env;
 use std::sync::{Arc, Mutex, mpsc};
 use std::io::{Read};
-use slog::DrainExt;
-use futures_cpupool::CpuPool;
 
 lazy_static! {
-    pub static ref CPU_POOL: CpuPool = {
-        CpuPool::new(4)
+    pub static ref LOG: slog::Logger = {
+        let decorator = slog_term::TermDecorator::new().build();
+        let drain = slog_term::FullFormat::new(decorator).build().fuse();
+        let drain = slog_async::Async::new(drain).build().fuse();
+        slog::Logger::root(drain, o!())
     };
 }
 
 fn main() {
-    let drain = slog_term::streamer().compact().build().fuse();
-    let root_log = slog::Logger::root(drain, o!("version" => env!("CARGO_PKG_VERSION")));
+    let root_log = LOG.clone();
     info!(root_log, "Initializing ffmpeg");
     if ffmpeg::init().is_err() {
         crit!(root_log, "FFmpeg could not be initialized!");
